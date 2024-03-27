6aa057076e23a92ae3167d7daaf70e16edf27679
Viktoriia3192 <vikysja9231@gmail.com>
2024-03-27 01:20:22 +0000

    com6

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,14 +1,14 @@
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
-    id: Joi.string().required(),
+    name: Joi.string().required(),
     email: Joi.string().required(),
     phone: Joi.string().required(),
     favorite: Joi.boolean(),
 })
 
 export const updateContactSchema = Joi.object({
-    id: Joi.string(),
+    name: Joi.string(),
     email: Joi.string(),
     phone: Joi.string(),
     favorite: Joi.boolean(),
